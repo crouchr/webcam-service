1955d218d1485ae5b4f93b04b40cd68d32883804
crouchr <richard.crouch100@gmail.com>
2021-02-07 19:08:01 +0000

    Bug fix

--- app/grab_still_from_video.py
+++ app/grab_still_from_video.py
@@ -1,4 +1,4 @@
-import integration_definitions
+import definitions
 
 
 def grab_still_from_video(mp4_filename):
@@ -17,7 +17,7 @@ def grab_still_from_video(mp4_filename):
 
 
 def main():
-    mp4_filename = integration_definitions.MEDIA_ROOT + 'test_images/test_video.mp4'
+    mp4_filename = definitions.MEDIA_ROOT + 'test_images/test_video.mp4'
     print('mp4_filename=' + mp4_filename)
 
     jpeg_filename = grab_still_from_video(mp4_filename)
